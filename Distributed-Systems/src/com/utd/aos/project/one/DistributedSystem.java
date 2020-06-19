0854825b66c9f9ddc47739426e82d7ae1ca5ca7b
indhiramudrageda <indhiramudrageda7@gmail.com>
2020-06-19 14:41:18 -0500

    Initial

---
+++ Distributed-Systems/src/com/utd/aos/project/one/DistributedSystem.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.utd.aos.project.one;
+
+/**
+ * @author indhiramudrageda
+ *
+ */
+public class DistributedSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
